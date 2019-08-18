d67481e11e979769a3b7b54d8f0a96165fcc190e
zhouliangze <2971609407@qq.com>
2019-08-18 14:24:12 +0800

    first commit

---
+++ src/main/java/com/example/springbootcommpent/SpringbootcommpentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcommpent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcommpentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootcommpentApplication.class, args);
+    }
+
+}
